9f326fdf2bcc4b1ad4517bc7a5398d0e369b1bba
August <august.enhager@gmail.com>
2021-09-27 03:06:41 +0200

    configures rspec

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 
 Bundler.require(*Rails.groups)
 
@@ -18,6 +18,12 @@ module UnterChallange
   class Application < Rails::Application
     config.load_defaults 6.1
     config.api_only = true
-    
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.skip_routes false
+      generate.controller_specs false
+      generate.request_specs false
+    end
   end
 end
